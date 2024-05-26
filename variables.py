67d8520f84ee4793f9f75a37e792495585d84130
Nitin Kumar <40369168+nitinkumar30@users.noreply.github.com>
2024-05-27 00:46:20 +0530

    Update variables.py
    Variables changed with dummy values

--- variables.py
+++ variables.py
@@ -1,10 +1,8 @@
 # VARIABLES
 URL_signup = "https://a.dff889.top/#/pages/login/reg?invit=U89900"
 URL_login = "https://a.dff889.top/#/pages/login/index"
-main_mail = "jawego1612@mcatag.com"
-main_pswd = "SharedByMyFriend"
-frnd1_mail = "dilemam228@fincainc.com"
-frnd1_pswd = main_pswd
-mail_from = "sudaim12@163.com"
-mail_subject = "DFF-football-Your Verification Code"
-mail_body = "This is your verification code: 5430"
+main_mail = "MASTER_ACCOUNT_MAIL"
+main_pswd = "MASTER_ACCOUNT_PSWD"
+frnd1_mail = "FRIEND1_ACCOUNT_MAIL"
+frnd1_pswd = "FRIEND1_ACCOUNT_PSWD"
+mail_from = "WEBSITE_ADMIN_MAIL_ID"
