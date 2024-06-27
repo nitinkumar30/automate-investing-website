2eeda3789996e2a86fe1dd9957ec698bda3941a5
Nitin Kumar <40369168+nitinkumar30@users.noreply.github.com>
2024-06-27 18:34:16 +0530

    Adjusted sensitive data for privacy

--- assets/variables.py
+++ assets/variables.py
@@ -1,10 +1,7 @@
 # VARIABLES
 URL_signup = "https://a.dff889.top/#/pages/login/reg?invit=U89900"
 URL_login = "https://a.dff889.top/#/pages/login/index"
-main_mail = "jawego1612@mcatag.com"
-main_pswd = "SharedByMyFriend"
-frnd1_mail = "dilemam228@fincainc.com"
-frnd1_pswd = main_pswd
-mail_from = "sudaim12@163.com"
-mail_subject = "DFF-football-Your Verification Code"
-mail_body = "This is your verification code: 5430"
+main_mail = "FIRST_MAIL"
+main_pswd = "FIRST_PASSWORD"
+frnd1_mail = "SECOND_MAIL"
+frnd1_pswd = "SECOND_PASSWORD"
