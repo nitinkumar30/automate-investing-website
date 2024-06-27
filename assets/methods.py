536b01a0f048cfc6d98bac951157c4ebd203408a
Nitin Kumar <40369168+nitinkumar30@users.noreply.github.com>
2024-06-27 18:35:20 +0530

    Deleted unnecessary methods not usable

--- assets/methods.py
+++ assets/methods.py
@@ -74,36 +74,3 @@ def extractOTPfromMailBody(mailBody):
     print("Verification Code:", otp)
     return otp
 
-
-# ---------------------------
-
-
-from tempmail import EMail
-
-email = EMail()
-
-
-def mail_address(email):
-    return email.address  # qwerty123@1secmail.com
-
-
-# ... request some email ...
-
-def getMessage(email):
-    msg = email.wait_for_message()
-    print("Subject of the ", msg.subject)
-    print("Body of the ", msg.body)
-
-    # ------- Getting messages in that mail
-
-    email = EMail(msg.body)
-    inbox = email.get_inbox()
-
-    for msg_info in inbox:
-        print(msg_info.subject, msg_info.message.body)
-
-        numbers = ''.join(c for c in msg_info.message.body if c.isdigit())
-        print("Verification Code:", numbers)
-        return numbers
-
-    # ------ Splitting otp from whole of message
